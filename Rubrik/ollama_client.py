cb71428ce8e21c987a69090dfe9ffaa2e737a97b
Sowjanya Vijayadurga <vijayadurga.panchakarla@unisys.com>
2026-07-01 23:08:26 -0700

    Create ollama_client.py

---
+++ Rubrik/ollama_client.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+OLLAMA_URL = os.getenv(
+    "OLLAMA_URL",
+    "http://ollama.ai-platform.svc.cluster.local:11434/api/generate"
+)
+
+MODEL = os.getenv(
+    "OLLAMA_MODEL",
+    "phi3:mini"
+)
+
+
+def ask_ollama(prompt):
+
+    payload = {
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False
+    }
+
+    response = requests.post(
+        OLLAMA_URL,
+        json=payload,
+        timeout=120
+    )
+
+    response.raise_for_status()
+
+    return response.json()["response"]
